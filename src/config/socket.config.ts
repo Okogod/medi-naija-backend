47cadbf4e3c9801872c1a98681b4b9bc21e797bf
Okogod <godspowerokoro17@gmail.com>
2026-03-20 16:05:37 +0100

    Install Socket.io and set up it configuration

---
+++ src/config/socket.config.ts
@@ -0,0 +1,32 @@
+import { Server as httpServer } from 'http';
+
+import express from 'express';
+
+import { Server as socketServer } from 'socket.io';
+
+
+const port = process.env.SERVER_PORT || 3000;
+
+
+const app = express();
+
+const http = new httpServer(app);
+
+const io = new socketServer(http);
+
+io.on( "connection", ( socket ) => {
+
+    socket.emit("connected");
+
+
+})
+
+
+http.listen( port, () => 
+    console.log(`socket server listening at: http://localhost:${port}`) 
+);
+
+export default io;
+
+
+
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
 
+import io from './config/socket.config.js';
+
 // Models
 import ModelInit from './models/model.init.js';
 
@@ -8,29 +10,21 @@ import ModelInit from './models/model.init.js';
 // Routes
 import PatientRouter from './patient/patient.routes.js';
 
-// Dotenv config
+// Dotenv Config
 dotenv.config();
 
 
 // Execute Models
 ModelInit();
 
-
-
-
+// Express App
 const app = express();
-
-app.get('/', (req, res) => {
-  res.json({ message: 'Welcome to the Patient Management API' });
-});
 
 app.use(express.json());
 
 app.use(PatientRouter);
 
 const PORT = process.env.SERVER_PORT ?? 3000;
-
-
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
